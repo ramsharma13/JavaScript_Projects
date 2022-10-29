3aad565b18a8fad4fdcf7ec3c718905e47ce68f6
Ravi Ranjan <90889370+Ravi4419@users.noreply.github.com>
2022-10-29 13:35:50 +0530

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Hactoberfest2022</h2>
+
+<p id="demo"></p>
+
+<script>
+
+let car = "Fiat";
+document.getElementById("demo").innerHTML = car;
+
+</script>
+
+</body>
+</html>
