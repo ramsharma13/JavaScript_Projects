c797331984d693c128ecbc97310a921dca744d55
Rakhi Kumari <92565649+lizza3010@users.noreply.github.com>
2022-10-29 14:37:25 +0530

    Create numbermethod.js

---
+++ numbermethod.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<p id="demo"></p>
+
+<script>
+let x = 9.656;
+document.getElementById("demo").innerHTML =
+  x.toExponential() + "<br>" + 
+  x.toExponential(2) + "<br>" + 
+  x.toExponential(4) + "<br>" + 
+  x.toExponential(6);
+</script>
+
+</body>
+</html>
