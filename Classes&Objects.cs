3f83d330dd588ede24eb4e5d92f820bbab98940a
developerJ001 <116951084+developerJ001@users.noreply.github.com>
2022-10-29 16:18:16 +0530

    Add files via upload

---
+++ Classes&Objects.cs
@@ -0,0 +1,18 @@
+
+//classes & Objects
+
+using System;
+
+namespace MyApplication
+{
+  class event
+  {
+    string prog = "Hactoberfest2022";
+
+    static void Main(string[] args)
+    {
+      event myObj = new Car();
+      Console.WriteLine(myObj.prog);
+    }
+  }
+}
